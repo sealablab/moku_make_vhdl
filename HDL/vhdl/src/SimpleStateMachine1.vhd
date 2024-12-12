bb795423add79e12137a6a3630174ac61344b118
johnycsh <sealablab@gmail.com>
2024-12-11 23:05:55 -0800

    added done

--- HDL/vhdl/src/SimpleStateMachine1.vhd
+++ HDL/vhdl/src/SimpleStateMachine1.vhd
@@ -7,20 +7,26 @@ entity SimpleStateMachine1 is
         Clk : in std_logic;
         Reset : in std_logic;
 
+    	-- Inputs
         Go : in std_logic;
-        Kill: in std_logic
+        Kill: in std_logic;
+
+	-- Outputs
+	Done: out std_logic
     );
 end entity;
 
+--the architecture block of SimpleStateMachine is for
+-- declaring our (local) variables. In this case, a 7-bit counter
 architecture Behavioural of SimpleStateMachine1 is
-    -- NOTE: This is an `enunmerated` type
-    type state_labels is ( Idle, Active, Finish, Abort);
+	signal TTL : unsigned(7 downto 0); -- 'TTL' read as: Time-to-live
+	type state_labels is ( Idle, Active, Finish, Abort); --Custom labels for our states
     signal my_state_reg : state_labels;
 begin 
 --perform cobinatorial assignments
 -- now time declare our counter process
 -- # def UpdateSimpleState(Clk)
-UpdateSimpleState: process(Clk)
+UpdateSimpleState_State: process(Clk)
 begin
 	if Reset = '1' then
 		my_state_reg <= Idle;
@@ -34,4 +40,18 @@ begin
 	end if; -- 'Reset'
 end process; -- (UpdateSimpleState)
 
-end architecture;
+Reg_proc: process(Clk)
+--The UpdateSimpleSTate_Reg process..
+--is responsible for manipulating
+begin
+	if Reset = '1' then
+	TTL <= X"00";
+	done <= '0';
+--	elsif rising_edge(Clk) then
+	-- inside this statement we are safe to update internal registers. right?
+
+	end if;
+end process;
+
+
+end architecture; -- SimpleStateMachine1
